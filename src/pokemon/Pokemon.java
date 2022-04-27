20e107d8c25a4e7bf37a425949c2969b61487444
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-04-27 09:15:21 +0200

    Actualizacion readme

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -3,6 +3,14 @@ package pokemon;
 public class Pokemon {
 
     private String nombre;
+    //TODO: poner todos los atributos y modificar el constructor
+
+    public Pokemon(String nombre){
+        this.nombre = nombre;
+    }
     
-    
+    public String getNombre() {
+        return nombre;
+    }
+
 }
