949ed38dfab809b7b305d6d2ac46d6ff2ef203e7
PedroAbad5 <pedro.s272655@cesurformacion.com>
2022-05-01 11:44:27 +0200

    Añadido métodos get, set y atacar

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,5 +1,6 @@
 package pokemon;
 
+
 public class Pokemon {
 
     //Atributos de la clase Pokemon
@@ -15,8 +16,9 @@ public class Pokemon {
     private int estamina;
     private int nivel;
     private int experiencia;
+    private int fertilidad;
     private Estado estado;
-    private Tipo tipo;
+    private Tipo[] tipo; //Máximo 2 tipos por pokemon
     /*Hasta que no se creen las clases, no se pueden añadir estas
     variables a la clase Pokemon. Añadirlas una vez creadas.
 
@@ -27,7 +29,7 @@ public class Pokemon {
     
     //TODO: poner todos los atributos y modificar el constructor
 
-    public Pokemon(String nombre, String mote, int vitalidad, int ataque, int defensa, int ataqueS, int defensaS, int velocidad, int estamina, int nivel){
+    public Pokemon(String nombre, String mote, int vitalidad, int ataque, int defensa, int ataqueS, int defensaS, int velocidad, int estamina, int nivel, Tipo[] tipo){
         this.nombre = nombre;
         this.mote= mote;
         this.nivel = nivel;
@@ -39,6 +41,96 @@ public class Pokemon {
         this.velocidad= velocidad;
         this.estamina= estamina;
         experiencia = 10 * nivel; //Experiencia necesaria para que un pokemon suba de nivel.
+        this.fertilidad = 5;
         this.estado = Estado.SIN_ESTADO;
+        this.tipo = tipo;
+
+    }
+
+    //Métodos get y set
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getMote() {
+        return mote;
+    }
+
+    public int getVitalidad() {
+        return vitalidad;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getAtaqueS() {
+        return ataqueS;
+    }
+
+    public int getDefensaS() {
+        return defensaS;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public int getEstamina() {
+        return estamina;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public int getFertilidad() {
+        return fertilidad;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public Tipo[] getTipo() {
+        return tipo;
+    }
+
+    /*
+    public Mejora getMejora() {
+        return mejora;
+    }
+
+    public Movimiento[] getMovimientos() {
+        return movimientos;
+    }
+    */
+
+    public void setMote(String mote) {
+        this.mote = mote;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    /*public void setMejora(Mejora mejora){
+        this.mejora = mejora;
+    }
+    */
+
+    //Métodos de acción
+
+    public void atacarPokemon(Pokemon pokemon){
+        
     }
 }
