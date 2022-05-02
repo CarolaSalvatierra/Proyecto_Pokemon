df2531902312e63f536f402bbc42f23722f57342
PedroAbad5 <pedro.s272655@cesurformacion.com>
2022-05-02 13:12:45 +0200

    Añadidas nuevas variables, creados métodos descansar y subirNivel

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,5 +1,6 @@
 package pokemon;
 
+import java.util.Random;
 
 public class Pokemon {
 
@@ -7,13 +8,15 @@ public class Pokemon {
 
     private String nombre;
     private String mote;
-    private int vitalidad;
+    private int vitalidadMax; //Vitalidad límite que puede tener el pokémon
+    private int vitalidad; //Vitalidad actual del pokémon.
     private int ataque;
     private int defensa;
     private int ataqueS; //Ataque Especial
     private int defensaS; //Defensa Especial
     private int velocidad;
-    private int estamina;
+    private int estaminaMax; //Estamina límite que puede tener el pokémon
+    private int estamina; //Estamina actual del pokémon.
     private int nivel;
     private int experiencia;
     private int fertilidad;
@@ -27,20 +30,23 @@ public class Pokemon {
 
     */
     
+    
     //TODO: poner todos los atributos y modificar el constructor
 
-    public Pokemon(String nombre, String mote, int vitalidad, int ataque, int defensa, int ataqueS, int defensaS, int velocidad, int estamina, int nivel, Tipo[] tipo){
+    public Pokemon(String nombre, String mote, int vitalidadMax, int ataque, int defensa, int ataqueS, int defensaS, int velocidad, int estaminaMax, int nivel, Tipo[] tipo){
         this.nombre = nombre;
         this.mote= mote;
         this.nivel = nivel;
-        this.vitalidad= vitalidad;
+        this.vitalidadMax= vitalidadMax;
+        this.vitalidad = this.vitalidadMax; //La vitalidad actual será igual a la máxima
         this.ataque = ataque;
         this.defensa = defensa;
         this.ataqueS = ataqueS;
         this.defensaS= defensaS;
         this.velocidad= velocidad;
-        this.estamina= estamina;
-        experiencia = 10 * nivel; //Experiencia necesaria para que un pokemon suba de nivel.
+        this.estaminaMax= estaminaMax;
+        this.estamina= this.estaminaMax; //La estamina actual será igual a la máxima
+        experiencia = 10 * nivel; //Experiencia necesaria para que un pokemon suba de nivel
         this.fertilidad = 5;
         this.estado = Estado.SIN_ESTADO;
         this.tipo = tipo;
@@ -55,6 +61,11 @@ public class Pokemon {
     
     public String getMote() {
         return mote;
+    }
+
+
+    public int getVitalidadMax() {
+        return vitalidadMax;
     }
 
     public int getVitalidad() {
@@ -79,6 +90,10 @@ public class Pokemon {
 
     public int getVelocidad() {
         return velocidad;
+    }
+
+    public int getEstaminaMax() {
+        return estaminaMax;
     }
 
     public int getEstamina() {
@@ -133,4 +148,37 @@ public class Pokemon {
     public void atacarPokemon(Pokemon pokemon){
         
     }
+
+    //Recupera toda su vitalidad y estamina, además de eliminar el estado que tuviese en ese momento.
+    public void descansar(Pokemon pokemon){
+        pokemon.setEstado(Estado.SIN_ESTADO);
+        this.vitalidad = this.vitalidadMax;
+        this.estamina= this.estaminaMax;
+    }
+
+    //Creo que esto podría hacerse de una forma más eficiente pero ahora mismo no tengo ni idea.
+    public void subirNivel(int exp){
+        boolean subirNivel = false; 
+        do{
+            this.experiencia -= exp;
+            if(this.experiencia <=0){
+                Random rm= new Random();
+                subirNivel = true;
+                exp = -this.experiencia; //Si la experiencia está por debajo de 0, significa que ha sobrado, así que la igualo aquí y la cambio a positivo
+                this.nivel += 1;
+                this.vitalidadMax += rm.nextInt(5) + 1;
+                this.estaminaMax += rm.nextInt(5) + 1;
+                this.ataque += rm.nextInt(5) + 1;
+                this.defensa += rm.nextInt(5) + 1;
+                this.ataqueS += rm.nextInt(5) + 1;
+                this.defensaS += rm.nextInt(5) + 1;
+                this.velocidad += rm.nextInt(5) + 1;
+                this.experiencia = 10 * nivel;
+                
+
+            }
+        }while (subirNivel == true);
+    }
+
+    //public void aprenderMovimiento(Pokemon pokemon, Movimiento movimiento);
 }
