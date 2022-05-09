3f04c35999c38e8e8ec8d1fb1446cd50d6147b07
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-05-09 19:31:06 +0200

    añadido escritura de fichre en el bufer

---
+++ src/combate/MainCombate.java
@@ -0,0 +1,18 @@
+package combate;
+
+public class MainCombate {
+
+    public static void main(String[] args) {
+        
+         //Creamos un combate
+         Combate c1 = new Combate();
+         Turno t1 = new Turno(1, "Charmander usa Ascuas","Squirtle usa Burbuja" );
+         //Añadimos el turno
+         c1.addTurno(t1);
+         c1.escribirCombate();
+
+
+
+    }
+    
+}
