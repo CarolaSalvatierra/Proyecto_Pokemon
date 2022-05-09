adf7a8df5fc9d1f0845aa4be96fff4fb1e0a52c6
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-05-09 21:19:58 +0200

    main de prueba para entrenador

---
+++ src/entrenador/MainEntrenador.java
@@ -0,0 +1,28 @@
+package entrenador;
+
+import pokemon.Pokemon;
+
+public class MainEntrenador {
+
+    public static void main(String[] args) {
+
+
+        Entrenador1 entr1 = new Entrenador1("Pep" );
+
+        Pokemon p1 = new Pokemon("pikachu1", "Pika1", 20, 10, 30, 15, 25, 30  , 50    , 20);
+
+        Pokemon p2 = new Pokemon("pikachu2", "Pika2", 20, 10, 30, 15, 25, 30  , 50    , 20);
+        
+        Pokemon p3 = new Pokemon("pikachu3", "Pika3", 20, 10, 30, 15, 25, 30  , 50    , 20);
+
+        Pokemon p4 = new Pokemon("pikachu4", "Pika4", 20, 10, 30, 15, 25, 30  , 50    , 20);
+        
+
+        
+
+       
+
+
+    }
+    
+}
