87b7d83d1a93946dd276baa0c15dc412d502fbda
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-05-11 22:26:16 +0200

    probando la clase encrenador

--- src/entrenador/MainEntrenador.java
+++ src/entrenador/MainEntrenador.java
@@ -1,5 +1,9 @@
 package entrenador;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import pokemon.Pokemon;
 
 public class MainEntrenador {
@@ -7,7 +11,8 @@ public class MainEntrenador {
     public static void main(String[] args) {
 
 
-        Entrenador1 entr1 = new Entrenador1("Pep" );
+      //  Entrenador1 entr1 = new Entrenador1("Pete", new ArrayList<Pokemon>(), new ArrayList<Pokemon>());
+        HashMap<String, LinkedList<Pokemon>>entrenador = new HashMap<String, LinkedList<Pokemon>>();
 
         Pokemon p1 = new Pokemon("pikachu1", "Pika1", 20, 10, 30, 15, 25, 30  , 50    , 20, null, null);
 
@@ -17,7 +22,11 @@ public class MainEntrenador {
 
         Pokemon p4 = new Pokemon("pikachu4", "Pika4", 20, 10, 30, 15, 25, 30  , 50    , 20, null, null);
         
-
+        entrenador.put("Tipo ", new LinkedList<Pokemon>());
+        entrenador.get("Tipo Planta ").add(p1);
+        entrenador.get("Tipo Planta ").add(p2);
+        entrenador.get("Tipo Planta ").add(p3);
+        entrenador.get("Tipo Planta ").add(p4);
         
 
        
