9d06f577b9e67c0583eb5358cbc714ca9feab0ac
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-05-20 13:04:04 +0200

    creación de clase inicioController

---
+++ src/controladores/InicioController.java
@@ -0,0 +1,37 @@
+package controladores;
+
+public class InicioController {
+//Aquí copio el codigo completo del anterior proyecto y lo mantengo comentado 
+//porque al importar la clase  FXML deja de ejecutar cualquiera de las otras clases
+
+/*
+public class InicioController implements Initializable {
+
+    @FXML
+    private Button btnInicioJuego;
+
+    @FXML
+    private Button imgInicio;
+
+    @FXML
+    private void inicioJuego(ActionEvent event) throws IOException {
+        Parent Inicio = FXMLLoader.load(getClass().getResource("../vistas/Inicio.fxml"));
+        Scene inicioScene = new Scene(Inicio);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(inicioScene);
+        window.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+    
+}
+
+*/
+ 
+    
+    
+}
