425516a6390e78bf931c830e0d14251451b2061f
carola_salvatierra <carola_salvatierra@hotmail.com>
2022-05-20 13:04:32 +0200

    creación de clase combate controller

---
+++ src/controladores/CombateController.java
@@ -0,0 +1,117 @@
+package controladores;
+
+public class CombateController {
+    
+    //Aquí copio el codigo completo del anterior proyecto y lo mantengo comentado 
+    //porque al importar la clase  FXML deja de ejecutar cualquiera de las otras clases
+    /*
+    public class CombateController implements Initializable {
+
+    @FXML
+    private Image imgFondo;
+
+    @FXML
+    private Image imgComBulbasur;
+
+    @FXML
+    private Image imgCombSquirtle;
+
+    @FXML
+    private Button btnMovimiento1;
+
+    @FXML
+    private Button btnMovimiento2;
+
+    @FXML
+    private Button btnMovimiento3;
+
+    @FXML
+    private Button btnMovimiento4;
+
+    @FXML
+    private Pokemon pokemonRival;
+
+    @FXML
+    private Pokemon pokemonJugador;
+
+    @FXML
+    private void movimiento1(ActionEvent event) {
+        try {
+            Parent combate = FXMLLoader.load(getClass().getResource("./vistas/Combate.fxml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        Tipo[] tipos = { Tipo.AGUA };
+        MovimientoAtaque mA = new MovimientoAtaque("Placaje", Tipo.NORMAL, TipoMovimiento.FISICO, 20, 55);
+
+        Movimiento[] movimientos = new Movimiento[4];
+        movimientos[0] = mA;
+        pokemonJugador.atacarPokemon(mA, pokemonRival)
+
+    }
+
+    @FXML
+    private void movimiento2(ActionEvent event) {
+        try {
+            Parent capturaView = FXMLLoader.load(getClass().getResource("./vistas/Combate.fxml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        Tipo[] tipos = { Tipo.AGUA };
+
+        MovimientoAtaque mA2 = new MovimientoAtaque("Burbuja", Tipo.AGUA, TipoMovimiento.ESPECIAL, 25, 10);
+
+        Movimiento[] movimientos = new Movimiento[3];
+        movimientos[1] = mA2;
+        pokemonJugador.atacarPokemon(mA2, pokemonRival)
+
+    }
+
+     @FXML
+    private void movimiento3(ActionEvent event) {
+        try {
+            Parent combate = FXMLLoader.load(getClass().getResource("./vistas/Combate.fxml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        Tipo[] tipos = { Tipo.AGUA };
+        MovimientoAtaque mA = new MovimientoAtaque("Placaje", Tipo.NORMAL, TipoMovimiento.FISICO, 20, 55);
+
+        Movimiento[] movimientos = new Movimiento[4];
+        movimientos[2] = mA3;
+        pokemonJugador.atacarPokemon(mA3, pokemonRival)
+
+    }
+
+    @FXML
+    private void movimiento4(ActionEvent event) {
+        try {
+            Parent capturaView = FXMLLoader.load(getClass().getResource("./vistas/Combate.fxml"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        Tipo[] tipos = { Tipo.AGUA };
+
+        MovimientoAtaque mA2 = new MovimientoAtaque("Burbuja", Tipo.AGUA, TipoMovimiento.ESPECIAL, 25, 10);
+
+        Movimiento[] movimientos = new Movimiento[3];
+        movimientos[3] = mA4;
+        pokemonJugador.atacarPokemon(mA4, pokemonRival)
+
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
+    */ 
+    
+}
